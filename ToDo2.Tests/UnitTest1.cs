1e8e7ce9405692ef42cc159492f4bcf4f69fccd8
madismatlik <pyhak666@gmail.com>
2019-08-27 16:05:42 +0300

    update test

--- ToDo2.Tests/UnitTest1.cs
+++ ToDo2.Tests/UnitTest1.cs
@@ -29,7 +29,23 @@ namespace ToDo2.Tests
             Assert.NotNull(task);
             Assert.Single(repo.GetAll());
             Assert.Equal("üks täsk", savedTask.Description);
-            }
+        }
+        [Fact]
+        public void CanUpdateTask()
+        {
+            IRepository<Task> repo = GetInMemoryTaskRepository();
+            Task task = new Task()
+            {
+                Id = 1,
+                Description = "üks täsk",
+                Status = true
+            };
+            Task savedTask = repo.Insert(task);
+            var desc = "Uuus kirjeldus";
+            savedTask.Description = desc;
+            repo.Update(savedTask);
+            Assert.Equal(desc, repo.Get(1).Description);
+        }
         private IRepository<Task> GetInMemoryTaskRepository()
         {
             DbContextOptions<ApiContext> options;
